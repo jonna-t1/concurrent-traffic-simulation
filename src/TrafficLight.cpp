e6efcc3e9547cef52b59fe37b0f83816afe1ef03
Jonna-t1 <j.turnbull1@outlook.com>
2022-08-20 19:18:19 +0100

    changes to send(); clearing _queue and using emplace_back

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -27,9 +27,10 @@ void MessageQueue<T>::send(T &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    _queue.clear();
     std::lock_guard<std::mutex> lock(_mutex);
     _condition.notify_one();
-    _queue.push_back(std::move(msg));
+    _queue.emplace_back(std::move(msg));
 }
 
 /* Implementation of class "TrafficLight" */
